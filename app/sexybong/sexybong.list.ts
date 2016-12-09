cd9f54d1d13f5abdad4433ef381f5e4dd4e10d7b
hoangpt <phantichhoang@gmail.com>
2016-12-09 23:54:20 +0700

    Tập 5: Mình phải thật ngon trong đêm nay

--- app/sexybong/sexybong.list.ts
+++ app/sexybong/sexybong.list.ts
@@ -6,6 +6,18 @@ import { SexyBong } from '../model/SexyBong';
 
 //service
 import { SexyBongService } from '../service/sexybong.service';
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
+
+//View Model
+export interface Filter {
+    filterBy: string;
+    currentPage: number;
+    perPage: number;
+    totalPage: number;
+}
+
+declare var $:any;
 
 //decorator
 @Component({
@@ -14,29 +26,52 @@ import { SexyBongService } from '../service/sexybong.service';
 })
 
 /**
- * public class
+ * View Controller
  */
 export class SexyBongList {
-    pageTitle: string = 'Product List';
+    pageTitle: string = 'Sexy List';
     widthImage: number = 50;
     showInfo: boolean = true;
-    filterBy: string = "Trang Phi";
+
+    //View Model
+    filter: Filter = {
+        filterBy: "Trang Phi",
+        currentPage: 1,
+        perPage: 2,
+        totalPage: 2
+    };
 
     public errorMessage: string;
 
     sexybongs: SexyBong[]; //access modifier (public, private, default)
+    subscription: Subscription;
+
+    fromNr: number = 0;
+    toNr: number = 1;
 
     //aggregration
     //DI
-    constructor(private _service: SexyBongService) {
+    constructor(private _service: SexyBongService, private _router: ActivatedRoute) {
     }
 
     //hook life cycle
     ngOnInit(): void {
+        //get page
+        this.subscription = this._router.params.subscribe(
+            (param: any) => {
+                let pageNr = param['page'];
+                if(pageNr){
+                    this.filter.currentPage = pageNr;
+                }
+                console.log(this.filter.currentPage);
+            }
+        );
+
         this._service.getSexyBong()
-        //observable
-        //arrow function
+            //observable
+            //arrow function
             .subscribe(sexybongs => {
+                    //get all sexybongs
                     this.sexybongs = sexybongs;
                     this._service.sexybongs = sexybongs;
 
@@ -46,6 +81,44 @@ export class SexyBongList {
                 () => {
                     console.log("Complete ok bebi");
                 });
+    }
+
+    ngAfterViewInit() : void {
+        //event using JS
+        $('#btnList').click(
+            () => {
+                $('#mode-card').hide();
+                $('#mode-list').show();
+            }
+        );
+
+        $('#btnCard').click(
+            () => {
+                $('#mode-card').show();
+                $('#mode-list').hide();
+            }
+        );
+
+        //logic page
+        if(this.sexybongs){
+            let totalSexyBong: number = this.sexybongs.length;
+            this.filter.totalPage = Math.ceil(totalSexyBong/this.filter.perPage);
+
+            //page1 -> 0,1
+            //page2 -> 2,3
+            this.fromNr = (this.filter.currentPage - 1)*this.filter.perPage;
+            this.toNr = this.fromNr + 1;
+            console.log(this.fromNr);
+            console.log(this.toNr);
+
+            //logic (continue)
+            //sexybongs -> sexybongs'
+        }
+    }
+
+    ngOnDestroy() : void {
+        //avoid memoryleak
+        this.subscription.unsubscribe();
     }
 
     fiveHundredInfo(): void {
